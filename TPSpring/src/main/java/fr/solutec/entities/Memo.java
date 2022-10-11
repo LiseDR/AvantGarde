d26bf2b0075e7aff510c12850b60d51e334db1ac
esic <esic@DESKTOP-B3SBA1F>
2022-10-11 14:55:21 +0200

    Changement dans MemoRepository

--- TPSpring/src/main/java/fr/solutec/entities/Memo.java
+++ TPSpring/src/main/java/fr/solutec/entities/Memo.java
@@ -5,6 +5,8 @@ import java.sql.Date;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -21,4 +23,7 @@ public class Memo {
 	private String memo;
 	@CreationTimestamp
 	private Date dateCreation;
+	
+	@ManyToOne
+	private User user;
 }
