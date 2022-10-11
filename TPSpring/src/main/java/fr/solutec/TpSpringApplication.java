5ff805715ed015b4a65fe3505763d42c94fda62a
esic <esic@DESKTOP-PNJK5JO>
2022-10-11 10:49:49 +0200

    Ajout du message de

--- TPSpring/src/main/java/fr/solutec/TpSpringApplication.java
+++ TPSpring/src/main/java/fr/solutec/TpSpringApplication.java
@@ -1,13 +1,18 @@
 package fr.solutec;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TpSpringApplication {
+public class TpSpringApplication implements CommandLineRunner  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TpSpringApplication.class, args);
+		System.out.println("Lancement terminé !");
 	}
-
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Lancement en cours !");
+	}
 }
